ceb51926bc80b2d9d75f4d67bccec7c1c86f71ea
Samueelx <gsammygitau@gmail.com>
2024-07-08 15:35:36 +0300

    Add login functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Home from "./pages/Home"
+// import Register from "./pages/Register";
+// import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <Home />
+   <Home />
   )
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import axiosInstance from "../api/axios";
+import { useNavigate } from "react-router-dom";
+import { AxiosError } from "axios";
+import { ErrorResponse } from "../types/ErrorResponse";
+
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await axiosInstance.post("/users/login", {
+        email,
+        password,
+      });
+      /**store token in localstorage */
+      localStorage.setItem("token", response.data.token);
+      console.log("Token stored: ", response.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        if(axiosError.response && axiosError.response.data){
+            setError(
+                axiosError.response?.data?.message || "An error occured. Please try again"
+              );
+        } else {
+            setError('An error occured. Please try again');
+        }
+      
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white shadow-md rounded-md">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        {error && <p className="mb-4 text-red-500">{error}</p>}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="email" className="block text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-gray-700">Password</label>
+            <input 
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
+          <button type="submit"
+          className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
