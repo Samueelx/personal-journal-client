cce68248afaf05cc909b9e27b624b89b9231b80a
Samueelx <gsammygitau@gmail.com>
2024-07-10 16:25:37 +0300

    Add functionality to add an entry.

--- src/pages/AddEntry.tsx
+++ src/pages/AddEntry.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../api/axios";
 import { ErrorResponse } from "../types/ErrorResponse";
 import { AxiosError } from "axios";
-import {format} from 'date-fns';
+import { format } from 'date-fns';
+import { fetchCategories } from "../api/categories";
 
 const AddEntry: React.FC = () => {
     const [title, setTitle] = useState<string>('');
@@ -12,7 +13,22 @@ const AddEntry: React.FC = () => {
     const [category, setCategory] = useState<string>('');
     const [date, setDate] = useState<string>('');
     const [error, setError] = useState<string>('');
+    const [categories, setCategories] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const getCategories = async () => {
+            try {
+                const data = await fetchCategories();
+                console.log("Categories: ",data)
+                setCategories(data);
+            } catch (error) {
+                console.log('Error fetching categories: ', error);
+            }
+        };
+
+        getCategories();
+    }, []);
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
@@ -21,16 +37,16 @@ const AddEntry: React.FC = () => {
         try {
             // const token = localStorage.getItem('token');
             const formattedDate = format(new Date(date), 'MM/dd/yyyy');
-            console.log({title, content, category, date: formattedDate});
+            console.log({ title, content, category, date: formattedDate });
             const response = await axiosInstance.post('/journals', {
                 title, content, category, date: formattedDate
             });
-            if(response.status === 201){
+            if (response.status === 201) {
                 navigate('/dashboard');
             }
         } catch (error) {
             const axiosError = error as AxiosError<ErrorResponse>;
-            if(axiosError.response && axiosError.response.data){
+            if (axiosError.response && axiosError.response.data) {
                 setError(axiosError?.response?.data?.message || 'Something went wrong')
                 console.log(axiosError?.response?.data);
             } else {
@@ -65,14 +81,17 @@ const AddEntry: React.FC = () => {
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-gray-700">Category</label>
-                        <input
-                            type="text"
-                            className="mt-1 block w-full px-4 py-2 border rounded-lg focus:ring focus:ring-opacity-50"
+                        <label className="block text-sm font-medium text-gray-700">Category</label>
+                        <select
                             value={category}
                             onChange={(e) => setCategory(e.target.value)}
-                            required
-                        />
+                            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                        >
+                            <option value="" disabled>Select a category</option>
+                            {categories.map((cat: any) => (
+                                <option key={cat.id} value={cat.name}>{cat.name}</option>
+                            ))}
+                        </select>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-700">Date</label>
